40ea107ff02f99478e4ba7fbaf237bbda2914842
Manmeet567 <74705756+Manmeet567@users.noreply.github.com>
2024-02-27 17:24:37 +0530

    Add files via upload

---
+++ routes/blogRoutes.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const { allBlogs, getBlogDetail, createBlog, updateBlog, deleteBlog, like, search, getBookmarks, addBookmark, removeBookmark } = require('../controllers/blogControllers.js');
+const requireAuth = require('../middlewares/requireAuth.js')
+
+
+const router = express.Router()
+
+// router.use(requireAuth);
+
+router.post('/blogs', allBlogs)
+
+router.post('/createBlog', requireAuth ,createBlog)
+
+router.post('/incomingLike', requireAuth,like)
+
+router.get('/blogs/:slug', getBlogDetail)
+
+router.patch('/updateBlog/:id', requireAuth ,updateBlog)
+
+router.get('/search-blogs/:query', search);
+
+router.delete('/deleteBlog/:id', requireAuth ,deleteBlog)
+
+router.get('/get-bookmarked-blogs', requireAuth, getBookmarks)
+
+router.post('/add-bookmark', requireAuth , addBookmark);
+
+router.post('/remove-bookmark', requireAuth, removeBookmark);
+
+module.exports = router;  
